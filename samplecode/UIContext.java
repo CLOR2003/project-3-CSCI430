91995acba2572237e0a45a5cf4242a5d8af87c11
CLOR2003 <93290045+CLOR2003@users.noreply.github.com>
2024-12-05 21:43:16 +0000

    Triangle drawing functionality added including buttons, calculations, and lines.

--- samplecode/UIContext.java
+++ samplecode/UIContext.java
@@ -3,4 +3,7 @@ public interface UIContext {
   //  public abstract void drawCircle(Circle circle);
   public abstract void drawLine(Point point1, Point point2 );
   public abstract void drawLabel(String s, Point p);
+
+  // For triangle
+  public abstract void drawTriangle(Point point1, Point point2, Point point3);
 }
